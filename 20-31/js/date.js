954835ebaf528a964c3835337b2010d62124eaa3
Zhumamidin Zholdoshov <zhumamidin.zholdoshov@gmail.com>
2023-12-19 17:43:16 +0545

    added new files

---
+++ 20-31/js/date.js
@@ -0,0 +1,37 @@
+/*
+////////////////////////////////////////
+let today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth();
+let day = today.getDate();
+let dayOfWeek = today.getDay();
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+let milliSecond = today.getMilliseconds();
+
+console.log(today);
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(dayOfWeek);
+console.log(hour);
+console.log(minute);
+console.log(second);
+console.log(milliSecond);
+
+
+////////////////////////////////////////
+*/
+
+/*
+////////////////////////////////////////
+
+let today = new Date();
+
+today.setDate(15);
+
+console.log(today);
+
+///////////////////////////////////////
+*/
---
+++ 20-31/js/functions.js
@@ -0,0 +1,6 @@
+function calcSum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+calcSum(10, 20);
+calcSum(20, 50);
