346dd396096dd4b1e4060b6fa175c02a906b27a7
Zhumamidin Zholdoshov <zhumamidin.zholdoshov@gmail.com>
2023-12-23 15:43:07 +0545

    finished project but without adaptive

--- leasing-cars/js/main.js
+++ leasing-cars/js/main.js
@@ -1,3 +1,29 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item')
+const tabContent = document.querySelectorAll('.tabs__content-item')
+
+tabItem.forEach(function (element) {
+	element.addEventListener('click', open)
+})
+
+function open(event) {
+	const tabTarget = event.currentTarget
+	const button = tabTarget.dataset.button
+
+	tabItem.forEach(function (item) {
+		item.classList.remove('tabs__btn-item--active')
+	})
+
+	tabTarget.classList.add('tabs__btn-item--active')
+
+	tabContent.forEach(function (item) {
+		item.classList.remove('tabs__content-item--active')
+	})
+
+	document
+		.querySelector(`#${button}`)
+		.classList.add('tabs__content-item--active')
+}
+
 const swiper = new Swiper('.swiper', {
 	effect: 'fade',
 	pagination: {
